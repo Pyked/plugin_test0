c5be6f81b2fe3b4764f755ec6798985e8715ec9b
Pyked <pybasket07@gmail.com>
2024-05-24 11:46:01 +0200

    nouveau

--- src/main/java/plugin_test/InefficientSort.java
+++ src/main/java/plugin_test/InefficientSort.java
@@ -1,5 +1,19 @@
 package plugin_test;
 
+import java.util.Arrays;
+
 public class InefficientSort {
-    
+    public static void main(String[] args) {
+        int[] array = {5, 3, 2, 4, 1};
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
 }
